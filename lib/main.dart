735a4f525000f19c875e3a593caf0626b079b6d3
Martin <bekzodsanakulov89@gmail.com>
2021-04-08 14:25:58 +0900

    feat: hard coding using Column NAWC as a body argument of Scoffold NAWC

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,35 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
+    var question = [
+      'What\'s your favorite programming language?',
+      'What\'s your favorite framework?',
+      'What\'s your favorite animals?',
+      'What\'s your favorite movie?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My Application'),
+          title: Text('My Application: Let\'s Have a Quiz'),
         ),
-        body: Text('This is my default text, HURRAY!'),
+        body: Column(
+          children: <Widget>[
+            Text('The Question!'),
+            RaisedButton(
+              child: Text('Answer one'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer two'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer three'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
