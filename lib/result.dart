a46737faa28c9ae8837e522886ebc66ecb3356d9
Martin <bekzodsanakulov89@gmail.com>
2021-04-20 20:49:48 +0900

    feat: using getters of Dart

--- lib/result.dart
+++ lib/result.dart
@@ -3,20 +3,24 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   final int totalScore;
 
+  String get resultPhrase {
+    String result;
+    if (this.totalScore > 25) {
+      result = 'Congrats, You are SUPER! ${this.totalScore}';
+    } else {
+      result = 'You are so so with score ${this.totalScore}';
+    }
+    return result;
+  }
+
   Result({this.totalScore});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: this.totalScore > 25
-          ? Text('Congrats, You are SUPER! ${this.totalScore}',
-              style: TextStyle(color: Colors.green, fontSize: 40),
-              textAlign: TextAlign.center)
-          : Text(
-              'You are so so with score ${this.totalScore}',
-              style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),
-              textAlign: TextAlign.center,
-            ),
+      child: Text(resultPhrase,
+          style: TextStyle(color: Colors.green, fontSize: 40),
+          textAlign: TextAlign.center),
     );
   }
 }
